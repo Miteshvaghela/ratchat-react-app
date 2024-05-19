e02bbf746d3e40d15c37d953ef20a7ff03150def
Miteshvaghela <vaghela_miteshk@yahoo.com>
2024-05-19 11:33:14 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,15 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+
 const port = process.env.PORT || 8080;
-const app = express(cors());
+const app = express();
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials : true,
+    methods : ['GET','POST']
+}));
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors : {
+        origin: 'http://localhost:5173',
+        credentials : true,
+        methods : ['GET','POST']
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log(
+        'User connected : ', socket.id
+    );
+});
 
 
-app.get('/', (req, res) => {
-    res.send('Node js server is up and running');
-})
-
-
-app.listen(port, () => {
-    console.log(`Application is running on ${port}`);
-})
+server.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
