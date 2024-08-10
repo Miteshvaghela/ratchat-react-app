b3a6e3715777ffd86bbd79d3cd441fa7878279b8
Miteshvaghela <vaghela_miteshk@yahoo.com>
2024-08-11 00:11:40 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,31 +1,26 @@
 import express from 'express';
 import http from 'http';
+//import cors from 'cors';
 import { Server } from 'socket.io';
-import cors from 'cors';
 
 
-const port = process.env.PORT || 4900;
 const app = express();
-// app.use(cors({
-//     origin: 'http://localhost:3000',
-//     credentials : true, 
-//     methods : ['GET', 'POST']
-// }));
 const server = http.createServer(app);
+const port = process.env.PORT || 8009;
 
 const io = new Server(server, {
     cors : {
-        origin : 'http://localhost:5173',
+        origin : "http://localhost:5173",
         credentials : true,
         methods : ['GET', 'POST']
     }
 });
 
 io.on('connection', (socket) => {
-    console.log('User connected : ', socket.id);
+    console.log('User connected ', socket.id);
 })
 
 
 server.listen(port, () => {
-    console.log('Server is running on ', port);
+    console.log(`Server is running on ${port}`);
 })
